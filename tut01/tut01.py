02a318cafd51e215123087b9c06cb9da02cda792
Veer Bahadur Singh <singhveerbahadur18@gmail.com>
2022-08-27 16:01:14 +0530

    Factorial code

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,5 +1,14 @@
-def factorial(int x):
-    
-
-x=int(input("Enter the number whose factorial is to be found"))
-factorial(x)
+def factorial(x):
+    if x < 0:
+        return 0
+    elif x == 0 or x == 1:
+        return 1
+    else:
+        facto = 1
+        while(x > 1):
+            facto *= x
+            x -= 1
+        return facto
+ 
+num = int(input("Enter the number whose factorial is to be found "))
+print("Factorial of",num,"is",factorial(num))
