0031aef2d9fb5a73d7b067e17c6ec7af5312b794
Veer Bahadur Singh <singhveerbahadur18@gmail.com>
2022-09-14 23:15:13 +0530

    Data preprocessing and finding difference

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -23,3 +23,15 @@ df['V Avg']=df['V Avg'].head(1)
 
 df['W Avg']=V_avg
 df['W Avg']=df['W Avg'].head(1)
+
+
+
+#here defined the X,Y,Z and X=U' , Y=V' , Z=W'
+X = df['U'] - U_avg
+Y = df['V'] - V_avg
+Z = df['W'] - W_avg
+
+#here made the column for storing X and named the column as U'=U - U_avg and similarly done for Y and Z.
+df["U'=U - U_avg"] = X
+df["V'=V - V_avg"] = Y
+df["W'=W - W_avg"] = Z
