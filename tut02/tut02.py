54af3f72722ef323830402b60da6a37584084805
Veer Bahadur Singh <singhveerbahadur18@gmail.com>
2022-09-30 23:41:35 +0530

    imported panda and accessing excel

---
+++ tut02/tut02.py
@@ -0,0 +1,5 @@
+#imported pandas library for accessing the input file 
+#then used shape to fetch the dimensions of pandas type object
+import pandas as pd
+df = pd.read_excel(r"C:\Users\hp\OneDrive\Documents\python\2001ME80_2022\tut02\input_octant_transition_identify.xlsx")
+x = df.shape[0]
