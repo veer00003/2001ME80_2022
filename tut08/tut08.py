2b42aa229ba873ffd2e1a95b774c24f557f4eea3
Veer Bahadur Singh <singhveerbahadur18@gmail.com>
2022-11-24 23:31:01 +0530

    Final solution with scorecard

--- tut08/tut08.py
+++ tut08/tut08.py
@@ -1,119 +1,127 @@
+from platform import python_version
 import re
 from datetime import datetime
 start_time = datetime.now()
 
-#Help
+# Help
+
+
 def scorecard():
-	#A dictionary having name of each player and short name used in commmentary
-	name = {'Bhuvneshwar':'Bhuvneshwar Kumar','Rahul':'KL Rahul', 'Rizwan':'Mohammad Rizwan(w)', 'Babar Azam':'Babar Azam(c)', 'Arshdeep Singh':'Arshdeep Singh', 'Fakhar Zaman':'Fakhar Zaman', 'Hardik Pandya':'Hardik Pandya', 'Avesh Khan':'Avesh Khan', 'Iftikhar Ahmed':'Iftikhar Ahmed', 'Chahal':'Yuzvendra Chahal', 'Jadeja':'Ravindra Jadeja', 'Khushdil':'Khushdil Shah', 'Shadab Khan':'Shadab Khan', 'Asif Ali':'Asif Ali', 'Mohammad Nawaz':'Mohammad Nawaz', 'Haris Rauf':'Haris Rauf', 'Naseem Shah':'Naseem Shah', 'Dahani':'Shahnawaz Dahani', 'Rohit':'Rohit Sharma(c)', 'Kohli':'Virat Kohli', 'Suryakumar Yadav':' Suryakumar Yadav', 'Karthik':'Dinesh Karthik(w)'}
-	#List of names of players of Team India
-	TeamIndia =[ 'Rohit','Rahul' ,'Kohli','Suryakumar Yadav', 'Karthik', 'Hardik Pandya', 'Jadeja', 'Bhuvneshwar', 'Avesh Khan', 'Chahal', 'Arshdeep Singh']
-	TeamPak = ['Babar Azam', 'Rizwan', 'Fakhar Zaman', 'Iftikhar Ahmed', 'Khushdil', 'Asif Ali', 'Shadab Khan', 'Mohammad Nawaz', 'Naseem Shah', 'Haris Rauf', 'Dahani']
-	#Opening the file
-	f = open('Scorecard.txt', 'w')
-	#Reading the innings file
-	f1 = open('pak_inns1.txt', 'r')
-	f2 = open('india_inns2.txt', 'r')
+    # A dictionary having name of each player and short name used in commmentary
+    name = {'Bhuvneshwar': 'Bhuvneshwar Kumar', 'Rahul': 'KL Rahul', 'Rizwan': 'Mohammad Rizwan(w)', 'Babar Azam': 'Babar Azam(c)', 'Arshdeep Singh': 'Arshdeep Singh', 'Fakhar Zaman': 'Fakhar Zaman', 'Hardik Pandya': 'Hardik Pandya', 'Avesh Khan': 'Avesh Khan', 'Iftikhar Ahmed': 'Iftikhar Ahmed', 'Chahal': 'Yuzvendra Chahal', 'Jadeja': 'Ravindra Jadeja',
+            'Khushdil': 'Khushdil Shah', 'Shadab Khan': 'Shadab Khan', 'Asif Ali': 'Asif Ali', 'Mohammad Nawaz': 'Mohammad Nawaz', 'Haris Rauf': 'Haris Rauf', 'Naseem Shah': 'Naseem Shah', 'Dahani': 'Shahnawaz Dahani', 'Rohit': 'Rohit Sharma(c)', 'Kohli': 'Virat Kohli', 'Suryakumar Yadav': ' Suryakumar Yadav', 'Karthik': 'Dinesh Karthik(w)'}
+    # List of names of players of Team India
+    TeamIndia = ['Rohit', 'Rahul', 'Kohli', 'Suryakumar Yadav', 'Karthik',
+                 'Hardik Pandya', 'Jadeja', 'Bhuvneshwar', 'Avesh Khan', 'Chahal', 'Arshdeep Singh']
+    TeamPak = ['Babar Azam', 'Rizwan', 'Fakhar Zaman', 'Iftikhar Ahmed', 'Khushdil',
+               'Asif Ali', 'Shadab Khan', 'Mohammad Nawaz', 'Naseem Shah', 'Haris Rauf', 'Dahani']
+    # Opening the file
+    f = open('Scorecard.txt', 'w')
+    # Reading the innings file
+    f1 = open('pak_inns1.txt', 'r')
+    f2 = open('india_inns2.txt', 'r')
 
-	#Reading all the lines
-	Pak = f1.readlines()
-	Ind = f2.readlines()
+    # Reading all the lines
+    Pak = f1.readlines()
+    Ind = f2.readlines()
 
-    	#Storing the number of balls thrown and run scored by each player in a dictionary 
-	#key as player short name and value as list
-	#Initializing
-	score = dict()
-	for i in name:
-		score[i] = [0,0]
-	
-	#Reaing Pakistan Innings
-	for i in Pak:
-		#If it contains sentences other than new line character
-		if(len(i.split(","))>1):
-			#taking the first part having
-			s1 = i.split(",")[0]
-			#Searching for name of bowler
-			#Pattern is it should be in between number 14.2 and 'to' 
-			#The below expression returns all such string
-			searchObj = re.search( r'(?:\d*\.\d+|\d+)(?: & b)? (.+?)(?: to |$)', s1, re.M|re.I)
-			bowler = searchObj.group(1)
+    # Storing the number of balls thrown and run scored by each player in a dictionary
+    # key as player short name and value as list
+    # Initializing
+    score = dict()
+    for i in name:
+        score[i] = [0, 0]
 
-			#The below expression returns all string from 'to' till end which is name of batsman
-			searchObj = re.search( r'to(?: & b)? (.+)', s1, re.M|re.I)
-			batsman = searchObj.group(1)
-			run = 0
+    # Reaing Pakistan Innings
+    for i in Pak:
+        # If it contains sentences other than new line character
+        if (len(i.split(",")) > 1):
+            # taking the first part having
+            s1 = i.split(",")[0]
+            # Searching for name of bowler
+            # Pattern is it should be in between number 14.2 and 'to'
+            # The below expression returns all such string
+            searchObj = re.search(
+                r'(?:\d*\.\d+|\d+)(?: & b)? (.+?)(?: to |$)', s1, re.M | re.I)
+            bowler = searchObj.group(1)
 
-			#Next string is for number of runs
-			s2 = i.split(",")[1].strip()
-			#doing stripping through space
+            # The below expression returns all string from 'to' till end which is name of batsman
+            searchObj = re.search(r'to(?: & b)? (.+)', s1, re.M | re.I)
+            batsman = searchObj.group(1)
+            run = 0
 
-			#if run 1,2,3 is scored, then converting to int and increasing the number of run
-			if(len(s2.split(" "))>1 and  s2.split(" ")[0].isnumeric()):
-				run = int(s2.split(" ")[0])
-			#And if it is a single expression, and 'FOUR' means 4 runs and 'SIX' means 6 runs scored
-			elif(s2.split(" ")[0] == 'FOUR'):
-				run = 4
-			elif(s2.split(" ")[0] == 'SIX'):
-				run = 6
+            # Next string is for number of runs
+            s2 = i.split(",")[1].strip()
+            # doing stripping through space
 
-			#Increasing amount of balls for bowler and runs for batsman
-			score[bowler][0] += 1
-			score[batsman][1] += run	
+            # if run 1,2,3 is scored, then converting to int and increasing the number of run
+            if (len(s2.split(" ")) > 1 and s2.split(" ")[0].isnumeric()):
+                run = int(s2.split(" ")[0])
+            # And if it is a single expression, and 'FOUR' means 4 runs and 'SIX' means 6 runs scored
+            elif (s2.split(" ")[0] == 'FOUR'):
+                run = 4
+            elif (s2.split(" ")[0] == 'SIX'):
+                run = 6
 
-	#Same above thing for India's inning
-	for i in Ind:
-		if(len(i.split(","))>1):
-			s1 = i.split(",")[0]
-			searchObj = re.search( r'(?:\d*\.\d+|\d+)(?: & b)? (.+?)(?: to |$)', s1, re.M|re.I)
-			bowler = searchObj.group(1)
-			searchObj = re.search( r'to(?: & b)? (.+)', s1, re.M|re.I)
-			batsman = searchObj.group(1)
-			run = 0
-			s2 = i.split(",")[1].strip()
-			if(len(s2.split(" "))>1 and  s2.split(" ")[0].isnumeric()):
-				run = int(s2.split(" ")[0])
-			elif(s2.split(" ")[0] == 'FOUR'):
-				run = 4
-			elif(s2.split(" ")[0] == 'SIX'):
-				run = 6
-			score[bowler][0] += 1
-			score[batsman][1] += run
+            # Increasing amount of balls for bowler and runs for batsman
+            score[bowler][0] += 1
+            score[batsman][1] += run
 
-	#Writing the data to txt file
-	f.write("Pakistan Innings....\n")
-	f.write("-"*59 + "\n")
+    # Same above thing for India's inning
+    for i in Ind:
+        if (len(i.split(",")) > 1):
+            s1 = i.split(",")[0]
+            searchObj = re.search(
+                r'(?:\d*\.\d+|\d+)(?: & b)? (.+?)(?: to |$)', s1, re.M | re.I)
+            bowler = searchObj.group(1)
+            searchObj = re.search(r'to(?: & b)? (.+)', s1, re.M | re.I)
+            batsman = searchObj.group(1)
+            run = 0
+            s2 = i.split(",")[1].strip()
+            if (len(s2.split(" ")) > 1 and s2.split(" ")[0].isnumeric()):
+                run = int(s2.split(" ")[0])
+            elif (s2.split(" ")[0] == 'FOUR'):
+                run = 4
+            elif (s2.split(" ")[0] == 'SIX'):
+                run = 6
+            score[bowler][0] += 1
+            score[batsman][1] += run
 
-	#Inserting name in left alignment with width 20
-	#Inserting bowls in middle alignment with width 15
-	#Inserting runs in right alignment with width 20
-	f.write(f"|{'Name of Player' : <20}|{'Bowls Thrown' : ^15}|{'Runs' : >20}|\n")
-	f.write("-"*59 +"\n")
-	for i in TeamPak:
-		f.write(f"|{name[i] : <20}|{score[i][0] : ^15}|{score[i][1] : >20}|\n")
-	f.write("-"*59 +"\n")
-	f.write("\n")
-	f.write("India Innings....\n")
-	f.write("-"*59 +"\n")
-	f.write(f"|{'Name of Player' : <20}|{'Bowls Thrown' : ^15}|{'Runs' : >20}|\n")
-	f.write('-'*59 +"\n")
-	for i in TeamIndia:
-		f.write(f"|{name[i] : <20}|{score[i][0] : ^15}|{score[i][1] : >20}|\n")
-	f.write('-'*59 +"\n")
-	f.write("\n")
-	f.write("End @\n")
-	f.write("Mandatory 0.1-6 38\n")
-	#saving the file
-	f.close()
+    # Writing the data to txt file
+    f.write("Pakistan Innings....\n")
+    f.write("-"*59 + "\n")
 
-###Code
+    # Inserting name in left alignment with width 20
+    # Inserting bowls in middle alignment with width 15
+    # Inserting runs in right alignment with width 20
+    f.write(f"|{'Name of Player' : <20}|{'Bowls Thrown' : ^15}|{'Runs' : >20}|\n")
+    f.write("-"*59 + "\n")
+    for i in TeamPak:
+        f.write(f"|{name[i] : <20}|{score[i][0] : ^15}|{score[i][1] : >20}|\n")
+    f.write("-"*59 + "\n")
+    f.write("\n")
+    f.write("India Innings....\n")
+    f.write("-"*59 + "\n")
+    f.write(f"|{'Name of Player' : <20}|{'Bowls Thrown' : ^15}|{'Runs' : >20}|\n")
+    f.write('-'*59 + "\n")
+    for i in TeamIndia:
+        f.write(f"|{name[i] : <20}|{score[i][0] : ^15}|{score[i][1] : >20}|\n")
+    f.write('-'*59 + "\n")
+    f.write("\n")
+    f.write("End @\n")
+    f.write("Mandatory 0.1-6 38\n")
+    # saving the file
+    f.close()
 
-from platform import python_version
+# Code
+
+
 ver = python_version()
 
 if ver == "3.8.10":
-	print("Correct Version Installed")
+    print("Correct Version Installed")
 else:
-	print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
 
 
 scorecard()
