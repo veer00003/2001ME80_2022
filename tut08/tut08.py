8366239ec0182bbf2425be340efc47c7cf42eb9a
Veer Bahadur Singh <singhveerbahadur18@gmail.com>
2022-11-24 23:25:54 +0530

    Innings of Pak

--- tut08/tut08.py
+++ tut08/tut08.py
@@ -18,3 +18,44 @@ def scorecard():
 	#Reading all the lines
 	Pak = f1.readlines()
 	Ind = f2.readlines()
+
+    	#Storing the number of balls thrown and run scored by each player in a dictionary 
+	#key as player short name and value as list
+	#Initializing
+	score = dict()
+	for i in name:
+		score[i] = [0,0]
+	
+	#Reaing Pakistan Innings
+	for i in Pak:
+		#If it contains sentences other than new line character
+		if(len(i.split(","))>1):
+			#taking the first part having
+			s1 = i.split(",")[0]
+			#Searching for name of bowler
+			#Pattern is it should be in between number 14.2 and 'to' 
+			#The below expression returns all such string
+			searchObj = re.search( r'(?:\d*\.\d+|\d+)(?: & b)? (.+?)(?: to |$)', s1, re.M|re.I)
+			bowler = searchObj.group(1)
+
+			#The below expression returns all string from 'to' till end which is name of batsman
+			searchObj = re.search( r'to(?: & b)? (.+)', s1, re.M|re.I)
+			batsman = searchObj.group(1)
+			run = 0
+
+			#Next string is for number of runs
+			s2 = i.split(",")[1].strip()
+			#doing stripping through space
+
+			#if run 1,2,3 is scored, then converting to int and increasing the number of run
+			if(len(s2.split(" "))>1 and  s2.split(" ")[0].isnumeric()):
+				run = int(s2.split(" ")[0])
+			#And if it is a single expression, and 'FOUR' means 4 runs and 'SIX' means 6 runs scored
+			elif(s2.split(" ")[0] == 'FOUR'):
+				run = 4
+			elif(s2.split(" ")[0] == 'SIX'):
+				run = 6
+
+			#Increasing amount of balls for bowler and runs for batsman
+			score[bowler][0] += 1
+			score[batsman][1] += run	
