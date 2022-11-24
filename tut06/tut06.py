c52176b02d7e6345f77ec0af74ee31f58be38ca1
Veer Bahadur Singh <singhveerbahadur18@gmail.com>
2022-11-24 19:27:32 +0530

    Final solution with output file

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -118,6 +118,8 @@ else:
 attendance_report()
 
 
+
+
 # This shall be the last lines of the code.
 end_time = datetime.now()
 print('Duration of Program Execution: {}'.format(end_time - start_time))
